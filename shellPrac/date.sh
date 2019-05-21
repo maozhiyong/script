3b3635c0734b5d100d9d9168641b4e334b3cffb2
maozhiyong <maozhiyong@jd.com>
2019-05-21 21:23:51 +0800

    date prac

---
+++ shellPrac/date.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+## 判断变量
+if ["$1" == ""]
+then
+    start_date=`date "+TIME%y-%m-%d"`
+    end_date=`date -v+10d "+TIME%y-%m-%d"`
+fi
+
+#echo $start_date
+#echo $end_date
+m=0
+for i in {1..100};
+do
+    curr=`date -v+"$i"d "+TIME%y-%m-%d"`
+    echo $curr
+    m=$(($m+1))
+    if [ "$curr" == "$end_date" ]
+    then
+        break
+    fi
+done
+echo $m
+
+
